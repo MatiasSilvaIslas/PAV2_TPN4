f1af19ed8acb9250fdcbf3086e15066e5cac82a9
Diego <diegotorresmauricio@gmail.com>
2024-10-02 22:26:03 -0300

    Inicialización y creación de clases base

---
+++ app/src/main/java/frgp/utn/edu/com/tabcontrol/clases/Articulo.java
@@ -0,0 +1,53 @@
+package frgp.utn.edu.com.tabcontrol.clases;
+
+import java.io.Serializable;
+
+public class Articulo implements Serializable {
+
+    private int id;
+    private String nombre;
+    private int stock;
+    private int idCategoria;
+
+    public Articulo() {}
+
+    public Articulo(int id, String nombre, int stock, int idCategoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.stock = stock;
+        this.idCategoria = idCategoria;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+}
